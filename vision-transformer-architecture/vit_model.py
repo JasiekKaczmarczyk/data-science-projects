54c07372f05c2fa7545a2004263649db32d5456c
JasiekKaczmarczyk <6trzesawid6@gmail.com>
2022-04-14 14:53:20 +0200

    Vision Transformer

---
+++ vision-transformer-architecture/vit_model.py
@@ -0,0 +1,130 @@
+from tkinter import image_names
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+from vit_patch_layers import Patches, PatchEncoder
+
+
+class VisionTransformer():
+
+    def __init__(self, input_shape, image_size, patch_size, projection_dim, num_transformer_layers, num_attention_heads, num_mlp_heads, output_classes) -> None:
+        self.input_shape = input_shape
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.projection_dim = projection_dim
+        self.num_transformer_layers = num_transformer_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_mlp_heads = num_mlp_heads
+        self.output_classes = output_classes
+
+        self.transformer_units = [projection_dim*2, projection_dim]
+        self.num_patches = None
+
+        self.model = self._build_architecture()
+
+    def _build_architecture(self):
+        # adding input layer
+        inputs = layers.Input(self.input_shape, name="Input Layer")
+
+        resized = layers.Resizing(self.image_size, self.image_size)(inputs)
+
+        # creating patches of image
+        patches = Patches(self.patch_size, name="Patch Layer")(resized)
+
+        # counting num of patches shape=(batch_size, num_patches, patch_dims)
+        self.num_patches = patches.shape[1]
+
+        # encoding patches
+        patches_encoded = PatchEncoder(self.num_patches, self.projection_dim, name="Patch Encoding Layer")(patches)
+
+        # transformer blocks
+        patches_encoded = self._add_transformer_layers(patches_encoded)
+
+        # creating [batch size, projection_dim] tensor
+        representation = self._add_representation_block(patches_encoded)
+
+        # add multi-layered-perceptron
+        features = self._add_mlp_block(representation, hidden_units=self.num_mlp_heads, dropout=0.5)
+
+        # classify outputs
+        outputs = layers.Dense(self.output_classes)(features)
+
+        return keras.Model(inputs=inputs, outputs=outputs)
+
+    def _add_mlp_block(self, x, hidden_units, dropout):
+        for units in hidden_units:
+            x = layers.Dense(units)(x)
+            x = tfa.layers.GELU()(x)
+            x = layers.Dropout(dropout)(x)
+        return x
+
+    def _add_transformer_layers(self, patches_encoded):
+
+        for i in range(self.num_transformer_layers):
+            # layer normalization
+            x1 = layers.LayerNormalization(epsilon=1e-6, name="LayerNormalization {i} Block")(patches_encoded)
+            # multi head attention
+            attention_output = layers.MultiHeadAttention(
+                num_heads=self.num_attention_heads, 
+                key_dim=self.projection_dim, 
+                dropout=0.1,
+                name=f"MultiHeadAttention Layer {i} Block"
+            )(x1, x1)
+
+            # skip connection
+            x2 = layers.Add()([attention_output, patches_encoded])
+
+            # layer normalization
+            x3 = layers.LayerNormalization(epsilon=1e-6, name="LayerNormalization {i} Block")(x2)
+
+            # mlp
+            x3 = self._add_mlp_block(x3, hidden_units=self.transformer_units, dropout=0.1)
+
+            # skip connection
+            patches_encoded=layers.Add()([x3, x2])
+        
+        return patches_encoded
+
+    def _add_representation_block(self, patches_encoded):
+        representation = layers.LayerNormalization(epsilon=1e-06, name="LayerNormalization")(patches_encoded)
+        representation = layers.Flatten(name="Flatten")(representation)
+        representation = layers.Dropout(0.5, name="Dropout")(representation)
+
+        return representation
+
+    def compile(self, learning_rate=0.001, weight_decay=0.0001):
+        """
+        Compiles model
+        """
+        optimizer=tfa.optimizers.AdamW(learning_rate=learning_rate, weight_decay=weight_decay)
+        self.model.compile(
+            optimizer=optimizer, 
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=[
+                keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                keras.metrics.SparseTopKCategoricalAccuracy(5, name="top-5-accuracy")
+            ]
+        )
+    
+    def fit(self, X, y, batch_size=32, epochs=100):
+        self.model.fit(x=X, y=y, batch_size=batch_size, epochs=epochs)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def evaluate(self, X_test, y_test):
+        return self.model.evaluate(X_test, y_test)
+    
+    def summary(self):
+        self.model.summary()
+
+    def save(self, save_dir="."):
+        self.model.save(filepath=save_dir)
+
+
+
+
+
+
+
---
+++ vision-transformer-architecture/vit_patch_layers.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+
+        self.patch_size=patch_size
+    
+    def call(self, images):
+        batch_size = images.shape[0]
+
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID"
+        )
+        patch_dims = patches.shape[-1]
+
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+
+        return patches
+
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+
+        self.num_patches = num_patches
+        # projection encoding layer
+        self.projection = layers.Dense(units=projection_dim)
+        # position encoding layer
+        self.position_embedding = layers.Embedding(input_dim=num_patches, output_dim=projection_dim)
+
+    def call(self, patches):
+        positions = tf.range(start=0, limit=self.num_patches)
+
+        return self.projection(patches) + self.position_embedding(positions)
